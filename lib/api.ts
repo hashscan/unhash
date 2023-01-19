20259796b39306af8a06ab61b47c43986ca1f94d
Alex Tkachenko <skyalexx@gmail.com>
2023-01-19 21:17:00 +0000

    Add price API method

--- lib/api.ts
+++ lib/api.ts
@@ -7,14 +7,24 @@ type DomainStatus = {
   isAvailable: boolean
 }
 
+export type DomainPrice = {
+  priceWei: string
+  priceEth: string
+}
+
 async function checkDomain(domain: string, network: Network = 'mainnet'): Promise<boolean> {
   const res = await ky.get(`${API_URL}/domain/check?domain=${domain}&network=${network}`).json<DomainStatus>()
   return res.isAvailable
 }
 
+async function getPrice(domain: string, network: Network = 'mainnet', duration: number): Promise<DomainPrice> {
+  return await ky.get(`${API_URL}/domain/price?domain=${domain}&network=${network}&duration=${duration}`)
+    .json<DomainPrice>()
+}
 
 const api = {
   checkDomain,
+  getPrice,
 }
 
 export default api
