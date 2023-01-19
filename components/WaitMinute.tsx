4d4a8f940e3269acdf6549ba327c623eaef13e94
v1rtl <pilll.pl22@gmail.com>
2023-01-19 18:23:25 +0200

    fix countdown

--- components/WaitMinute.tsx
+++ components/WaitMinute.tsx
@@ -4,6 +4,7 @@ import { useLocalStorage, useReadLocalStorage } from 'usehooks-ts'
 import { useProvider } from 'wagmi'
 import { ProgressBar } from './icons'
 import ui from 'styles/ui.module.css'
+import { useCountdown } from 'lib/hooks/useCountdown'
 
 export const WaitMinute = () => {
   const [isReady, setReady] = useState(false)
@@ -11,49 +12,32 @@ export const WaitMinute = () => {
   const commitTxBlock = useReadLocalStorage<number>('commit-tx-block')
   const [_, setStep] = useLocalStorage<RegistrationStep>('step', 'wait')
   const [date, setDate] = useState<Date>(new Date(0))
-  const [minutes, setMinutes] = useState(0)
-  const [seconds, setSeconds] = useState(0)
-  useEffect(() => {
-    const mins = () => {
-      const now = new Date()
 
-      setMinutes(now.getMinutes() - date.getMinutes())
-      setSeconds(now.getSeconds() - date.getSeconds())
-      if (minutes >= 1 && seconds >= 1) {
-        setReady(true)
-      }
-    }
-    mins()
-    const iv = setInterval(mins, 1000)
-    if (isReady) clearInterval(iv)
-    return () => clearInterval(iv)
-  }, [date, isReady])
+  const [days, hours, minutes, seconds] = useCountdown(date)
 
   useEffect(() => {
     provider.getBlock(commitTxBlock!).then((block) => {
-      setDate(new Date(block.timestamp * 1000))
+      setDate(new Date(block.timestamp * 1000 + 60000)) // timestamp + 1 minute
     })
   }, [commitTxBlock, provider])
 
-  return (
-    <>
-      {isReady ? (
-        <button
-          className={ui.button}
-          onClick={() => {
-            setStep('register')
-          }}
-        >
-          Next
-        </button>
-      ) : (
-        <div>
-          <p>Seconds left: {60 - seconds}</p>
-          <p>
-            Wait a minute <ProgressBar color="var(--text-primary)" />
-          </p>
-        </div>
-      )}
-    </>
-  )
+  if (days + hours + minutes + seconds <= 0) {
+    return (
+      <button
+        className={ui.button}
+        onClick={() => {
+          setStep('register')
+        }}
+      >
+        Next
+      </button>
+    )
+  } else {
+    return (
+      <div>
+        <p>Wait {seconds} seconds</p>
+        <ProgressBar color="var(--text-primary)" />
+      </div>
+    )
+  }
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -13,7 +13,8 @@ export const ETH_REGISTRAR_ABI = [
   'function available(string) view returns (bool)',
   'function makeCommitment(string,address,bytes32) pure returns (bytes32)',
   'function commit(bytes32)',
-  'function register(string,address,uint256,bytes32) payable'
+  'function register(string,address,uint256,bytes32) payable',
+  'function registerWithConfig(string memory name, address owner, uint duration, bytes32 secret, address resolver, address addr) public payable'
 ]
 
 export const YEAR_IN_SECONDS = 365 * 60 * 60 * 24
