8c81a236929e97082b2081e289a15305fcea35bd
Alex Tkachenko <skyalexx@gmail.com>
2023-01-25 21:17:07 +0000

    Domain info API method (records only)

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import ky from "ky"
 import { API_URL } from "./constants"
-import { Network } from "./types"
+import { Network, Fields } from "./types"
 
 type DomainStatus = {
   domain: string
@@ -13,6 +13,10 @@ export type DomainPrice = {
   usd: number
 }
 
+export type DomainInfo = {
+  records: Fields
+}
+
 async function checkDomain(domain: string, network: Network = 'mainnet'): Promise<boolean> {
   const res = await ky.get(`${API_URL}/domain/check?domain=${domain}&network=${network}`).json<DomainStatus>()
   return res.isAvailable
@@ -23,9 +27,16 @@ async function getPrice(domain: string, network: Network = 'mainnet', duration: 
     .json<DomainPrice>()
 }
 
+async function domainInfo(domain: string, network: Network = 'mainnet'): Promise<Fields> {
+  const res = await ky.get(`${API_URL}/domain/info?domain=${domain}&network=${network}`)
+    .json<DomainInfo>()
+  return res.records
+}
+
 const api = {
   checkDomain,
   getPrice,
+  domainInfo,
 }
 
 export default api
