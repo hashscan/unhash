1f0ca8be7ab0e850301194431c89d6485a174bb3
v1rtl <pilll.pl22@gmail.com>
2023-01-31 19:54:40 +0200

    bump deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,41 @@ import { Nav } from 'components/Nav'
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider,
+  provider
 })
 
 const rainbowkitTheme: Theme = {
   ...darkTheme(),
-  fonts: { body: 'var(--font-ui)' },
+  fonts: { body: 'var(--font-ui)' }
 }
+
+// primary Sans Serif font for the UI
+const Lausanne = localFont({
+  src: [
+    {
+      path: '../styles/fonts/Lausanne/Lausanne-200.woff2',
+      weight: '200'
+    },
+    {
+      path: '../styles/fonts/Lausanne/Lausanne-300.woff2',
+      weight: '300'
+    },
+    {
+      path: '../styles/fonts/Lausanne/Lausanne-400.woff2',
+      weight: '400'
+    },
+    {
+      path: '../styles/fonts/Lausanne/Lausanne-500.woff2',
+      weight: '500'
+    },
+    {
+      path: '../styles/fonts/Lausanne/Lausanne-600.woff2',
+      weight: '600'
+    }
+  ],
+  fallback: ['system-ui'],
+  display: 'swap'
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -40,33 +68,5 @@ const App = ({ Component, pageProps }: AppProps) => {
     </>
   )
 }
-
-// primary Sans Serif font for the UI
-const Lausanne = localFont({
-  src: [
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-200.woff2',
-      weight: '200',
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-300.woff2',
-      weight: '300',
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-400.woff2',
-      weight: '400',
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-500.woff2',
-      weight: '500',
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-600.woff2',
-      weight: '600',
-    },
-  ],
-  fallback: ['system-ui'],
-  display: 'swap',
-})
 
 export default App
