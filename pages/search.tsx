20d823399666a3a76ea738347038eb894287b1f3
Alexey Taktarov <molefrog@gmail.com>
2023-02-01 15:34:01 +0100

    Grid pattern background for the hero block.

--- pages/search.tsx
+++ pages/search.tsx
@@ -13,33 +13,39 @@ interface SearchProps {}
 
 const Search: NextPage<SearchProps> = (props) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.hero}>
-        <h1 className={styles.heroTitle}>
-          <WrapBalancer>Get Your Unique .eth Domain</WrapBalancer>
-        </h1>
+    <>
+      <div className={styles.heroSection}>
+        <div className={styles.container}>
+          <div className={styles.hero}>
+            <h1 className={styles.heroTitle}>
+              <WrapBalancer>Get Your Unique .eth Domain</WrapBalancer>
+            </h1>
 
-        <h2 className={styles.heroSubtitle}>
-          <WrapBalancer>
-            A modern and better way of searching, buying and managing{' '}
-            <b>ENS domains</b>
-          </WrapBalancer>
-        </h2>
-      </div>
-
-      <div className={styles.search}>
-        <input
-          autoFocus
-          className={styles.searchInput}
-          spellCheck="false"
-          placeholder="Look up .eth domain..."
-        ></input>
-
-        <div className={styles.action}>
-          <SearchButton status={SearchStatus.Available} />
+            <h2 className={styles.heroSubtitle}>
+              <WrapBalancer>
+                A modern and better way of searching, buying and managing{' '}
+                <b>ENS domains</b>
+              </WrapBalancer>
+            </h2>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className={styles.container}>
+        <div className={styles.search}>
+          <input
+            autoFocus
+            className={styles.searchInput}
+            spellCheck="false"
+            placeholder="Look up .eth domain..."
+          ></input>
+
+          <div className={styles.action}>
+            <SearchButton status={SearchStatus.Available} />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
