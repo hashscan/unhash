dedb34ac6a9f97516fbc691bf8908d2e3dbe4943
Alexey Taktarov <molefrog@gmail.com>
2023-02-04 17:29:34 +0100

    Implement basic <Footer /> component.

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.appName}>ens-wallets.com</div>
+
+      {/*
+        What really this part of the footer should contain? Links!
+
+        Links to our own content pages that are important for the SEO, for example
+        "Get .eth domain" or "What is .eth?" (these are real Google
+        search queries that are currently raising popularity)
+
+        We can also add links to some popular .eth names, e.g. vitalik.eth or jack.eth
+        (they appear in Google Trends as well)
+      */}
+      <div className={styles.links}>
+        we run on top of{' '}
+        <a href="https://docs.ens.domains/">Etherium Name Service↗</a>
+      </div>
+
+      {/* Other ideas: gas price, current block, number of .ETH registered in 24h... */}
+    </footer>
+  )
+}
