890bf36db02c725222a07f8d8b96935cf1558273
Alexey Taktarov <molefrog@gmail.com>
2023-02-04 18:00:57 +0100

    Static list of suggested names.

--- components/LandingPricing/LandingPricing.tsx
+++ components/LandingPricing/LandingPricing.tsx
@@ -10,7 +10,7 @@ export const LandingPricing = () => {
 
         <div className={styles.explainer}>
           The base price is determined by the length of the name and the
-          registration period, plus the Ethereum network fees<sup>*</sup>
+          registration period, plus the Ethereum network fees.
         </div>
       </div>
 
@@ -20,7 +20,8 @@ export const LandingPricing = () => {
           <tr>
             <td>
               <span className={styles.price}>
-                $5<small> / year</small>
+                $5
+                <small> / year</small>
               </span>
             </td>
             <td>5+ character names</td>
@@ -32,18 +33,42 @@ export const LandingPricing = () => {
                 $160<small> / year</small>
               </span>
             </td>
-            <td>4 character names, e.g. jack.eth</td>
+            <td>
+              4 character names, e.g.{' '}
+              <a
+                href="https://nick.eth.co/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                nick.eth
+              </a>
+            </td>
           </tr>
 
           <tr>
             <td>
               <span className={styles.price}>
-                $640<small> / year</small>
+                $640<sup>*</sup>
+                <small> / year</small>
               </span>
             </td>
-            <td>3 character names, e.g. isc.eth</td>
+            <td>
+              3 character names, e.g.{' '}
+              <a
+                href="https://isc.eth.co/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                isc.eth
+              </a>
+            </td>
           </tr>
         </table>
+
+        <div className={styles.subnote}>
+          <sup>*</sup> Domain renewal fee is automatically converted to ETH on
+          the time of the transaction.
+        </div>
       </div>
     </div>
   )
