aa2c74ea72ecd91fd47a33b5fbbe55f0cfae94b9
Alex Tkachenko <skyalexx@gmail.com>
2023-02-04 19:07:22 +0000

    Wait logic & ui prototype

--- components/CheckoutRegisterStep/CheckoutRegisterStep.tsx
+++ components/CheckoutRegisterStep/CheckoutRegisterStep.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import { CheckoutWait } from 'components/CheckoutWait'
+import { useCountdown } from 'lib/hooks/useCountdown'
+import React, { useState } from 'react'
 import styles from './CheckoutRegisterStep.module.css'
 
+
+const WAIT_TIME_S = 5
 
 interface CheckoutRegisterStepProps {
   domain: string
 }
 
 export const CheckoutRegisterStep = (props: CheckoutRegisterStepProps) => {
-  // TODO: add waiting form if time not passed
+  // emulate recent commit tx timestamp
+  const [commitedAt,] = useState(Date.now())
+
+  const count = useCountdown(commitedAt + WAIT_TIME_S * 1000)
+  const wait = count > 0
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>Confirm registration</div>
-      <div className={styles.subheader}>Confirm below to register your domain and configure the profile</div>
+      {wait
+        ? (
+          <CheckoutWait value={count} />
+        )
+        : (<>
+          <div className={styles.header}>Confirm registration</div>
+          <div className={styles.subheader}>Confirm below to register your domain and configure the profile</div>
+        </>)
+      }
     </div>
   )
 }
