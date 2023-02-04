c3972b860a153c5612005e6b68c04f18366c1cae
Alex Tkachenko <skyalexx@gmail.com>
2023-02-04 14:43:24 +0000

    PR review: use svg current color

--- components/icons.tsx
+++ components/icons.tsx
@@ -62,12 +62,12 @@ export const EthereumIcon = () => (
       <metadata id="CorelCorpID_0Corel-Layer" />
       <g id="_1421394342400">
         <g>
-          <polygon fill="var(--color-slate-f)" fill-rule="nonzero" points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54 " />
-          <polygon fill="var(--color-slate-f)" fill-rule="nonzero" points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33 " />
-          <polygon fill="var(--color-slate-f)" fill-rule="nonzero" points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89 " />
-          <polygon fill="var(--color-slate-f)" fill-rule="nonzero" points="392.07,1277.38 392.07,956.52 -0,724.89 " />
-          <polygon fill="var(--color-slate-f)" fill-rule="nonzero" points="392.07,882.29 784.13,650.54 392.07,472.33 " />
-          <polygon fill="var(--color-slate-f)" fill-rule="nonzero" points="0,650.54 392.07,882.29 392.07,472.33 " />
+          <polygon fill="currentColor" fill-rule="nonzero" points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54 " />
+          <polygon fill="currentColor" fill-rule="nonzero" points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33 " />
+          <polygon fill="currentColor" fill-rule="nonzero" points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89 " />
+          <polygon fill="currentColor" fill-rule="nonzero" points="392.07,1277.38 392.07,956.52 -0,724.89 " />
+          <polygon fill="currentColor" fill-rule="nonzero" points="392.07,882.29 784.13,650.54 392.07,472.33 " />
+          <polygon fill="currentColor" fill-rule="nonzero" points="0,650.54 392.07,882.29 392.07,472.33 " />
         </g>
       </g>
     </g>
