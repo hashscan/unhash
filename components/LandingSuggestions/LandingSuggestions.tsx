f1247d359352a4497d27086ebe1c473a9ff79340
Alexey Taktarov <molefrog@gmail.com>
2023-02-05 11:42:02 +0100

    Implement <LandingSuggestions /> using naive name generation.

---
+++ components/LandingSuggestions/LandingSuggestions.tsx
@@ -0,0 +1,68 @@
+import {
+  ComponentProps,
+  useInsertionEffect,
+  useLayoutEffect,
+  useState
+} from 'react'
+import styles from './LandingSuggestions.module.css'
+
+import { type Suggestion } from './types'
+import { generateUsername } from 'unique-username-generator'
+
+// Naive domain suggester
+// Migrate to better server-side suggestion algorithm
+const generateSuggestion = (): Suggestion => {
+  return {
+    domain: `${generateUsername()}.eth`,
+    priceInUSD: 5 // TODO
+  }
+}
+
+interface SuggestionTagProps extends ComponentProps<'div'> {
+  suggestion: Suggestion
+}
+
+const SuggestionTag = ({
+  suggestion: { domain, priceInUSD },
+  ...props
+}: SuggestionTagProps) => {
+  return (
+    <div className={styles.suggestion} {...props}>
+      {domain}
+      <span>${priceInUSD}</span>
+    </div>
+  )
+}
+
+export interface LandingSuggestionsProps {
+  onSuggestionSelected: (s: Suggestion) => void
+}
+
+export const LandingSuggestions = ({
+  onSuggestionSelected
+}: LandingSuggestionsProps) => {
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([])
+
+  useLayoutEffect(() => {
+    setSuggestions(
+      Array(6)
+        .fill(0)
+        .map((_) => generateSuggestion())
+    )
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Suggested for you</div>
+      <div className={styles.suggestions}>
+        {suggestions.map((item) => (
+          <SuggestionTag
+            key={item.domain}
+            suggestion={item}
+            onClick={() => onSuggestionSelected(item)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
