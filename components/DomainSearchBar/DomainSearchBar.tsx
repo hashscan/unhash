1c7753c6e0a36518e0dfbaaa315685e8be793cc6
Alexey Taktarov <molefrog@gmail.com>
2023-02-05 11:50:20 +0100

    Wire up search bar and suggestions together.

--- components/DomainSearchBar/DomainSearchBar.tsx
+++ components/DomainSearchBar/DomainSearchBar.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, {
+  forwardRef,
+  useEffect,
+  useState,
+  useImperativeHandle
+} from 'react'
 
 import { SearchButton, SearchStatus } from './SearchButton'
 import styles from './DomainSearchBar.module.css'
@@ -7,44 +12,62 @@ import { useChainId } from 'wagmi'
 import { validateDomain } from 'lib/utils'
 import { useDebounce } from 'usehooks-ts'
 
-export const DomainSearchBar = () => {
-  /**
-   * TODO: this is just a naive mock implementation
-   */
-  const [searchQuery, setSearchQuery] = useState('')
-  const [status, setStatus] = useState(SearchStatus.Loading)
-  const chainId = useChainId()
-
-  const domainInput = useDebounce(searchQuery, 300)
-
-  useEffect(() => {
-    if (domainInput.length === 0) {
-      setStatus(SearchStatus.Inactive)
-    } else {
-      if (validateDomain(domainInput)) return
-      const check = async () => {
-        setStatus(SearchStatus.Loading)
-        const available = await api.checkDomain(domainInput, chainId === 1 ? 'mainnet' : 'goerli')
-        setStatus(available ? SearchStatus.Available : SearchStatus.NotAvailable)
-      }
-      check()
-    }
-  }, [domainInput, chainId])
-
-  return (
-    <div className={styles.searchBar}>
-      <input
-        autoFocus
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className={styles.input}
-        spellCheck="false"
-        placeholder="Look up .eth domain..."
-      ></input>
-
-      <div className={styles.action}>
-        <SearchButton status={status} />
-      </div>
-    </div>
-  )
+// allow parent components to imperatively update search string using ref
+export type SearchBarHandle = {
+  setSearch: (val: string) => void
 }
+
+export const DomainSearchBar = forwardRef<SearchBarHandle, {}>(
+  (_props, ref) => {
+    /**
+     * TODO: this is just a naive mock implementation
+     */
+    const [searchQuery, setSearchQuery] = useState('')
+    const [status, setStatus] = useState(SearchStatus.Loading)
+    const chainId = useChainId()
+
+    const domainInput = useDebounce(searchQuery, 300)
+
+    useImperativeHandle(ref, () => ({
+      setSearch(value: string) {
+        setSearchQuery(value)
+      }
+    }))
+
+    useEffect(() => {
+      if (domainInput.length === 0) {
+        setStatus(SearchStatus.Inactive)
+      } else {
+        if (validateDomain(domainInput)) return
+        const check = async () => {
+          setStatus(SearchStatus.Loading)
+          const available = await api.checkDomain(
+            domainInput,
+            chainId === 1 ? 'mainnet' : 'goerli'
+          )
+          setStatus(
+            available ? SearchStatus.Available : SearchStatus.NotAvailable
+          )
+        }
+        check()
+      }
+    }, [domainInput, chainId])
+
+    return (
+      <div className={styles.searchBar}>
+        <input
+          autoFocus
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className={styles.input}
+          spellCheck="false"
+          placeholder="Look up .eth domain..."
+        ></input>
+
+        <div className={styles.action}>
+          <SearchButton status={status} />
+        </div>
+      </div>
+    )
+  }
+)
