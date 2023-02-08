c876d80885dee4ca216c069094fdff467551040f
Alexey Taktarov <molefrog@gmail.com>
2023-02-08 09:57:26 +0100

    Reorg fonts, add JetBrainsMono (Google Fonts) as a monospace font.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import { createClient, WagmiConfig } from 'wagmi'
 
 import '@rainbow-me/rainbowkit/styles.css'
 import 'styles/global.css'
-import localFont from '@next/font/local'
 import { AppProps } from 'next/app'
 import { chains, provider, connectors } from 'lib/connectors'
 import { wrapInLayout } from 'components/layouts'
+
+import { Lausanne, JetBrainsMono } from 'styles/fonts'
 
 const wagmiClient = createClient({
   autoConnect: true,
@@ -19,34 +20,6 @@ const rainbowkitTheme: Theme = {
   fonts: { body: 'var(--font-ui)' }
 }
 
-// primary Sans Serif font for the UI
-const Lausanne = localFont({
-  src: [
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-200.woff2',
-      weight: '200'
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-300.woff2',
-      weight: '300'
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-400.woff2',
-      weight: '400'
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-500.woff2',
-      weight: '500'
-    },
-    {
-      path: '../styles/fonts/Lausanne/Lausanne-600.woff2',
-      weight: '600'
-    }
-  ],
-  fallback: ['system-ui'],
-  display: 'swap'
-})
-
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
@@ -54,7 +27,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         {`
           :root {
             --font-ui: ${Lausanne.style.fontFamily};
-            --font-mono: monospace;
+            --font-mono: ${JetBrainsMono.style.fontFamily};
           }
         `}
       </style>
