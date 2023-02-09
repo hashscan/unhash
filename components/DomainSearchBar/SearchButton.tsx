48bb466df92e4eba5f65df3169831259c32899ce
Alexey Taktarov <molefrog@gmail.com>
2023-02-09 10:20:01 +0100

    Proper navigation state.

--- components/DomainSearchBar/SearchButton.tsx
+++ components/DomainSearchBar/SearchButton.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from 'react'
+import React, { ComponentProps, MouseEventHandler } from 'react'
 
 import clsx from 'clsx'
 import styles from './SearchButton.module.css'
@@ -7,26 +7,33 @@ import { LoaderHorseshoe } from 'components/icons'
 
 export interface SearchButtonProps extends ComponentProps<'div'> {
   status: SearchStatus
-  focused: boolean
+  isNavigating: boolean
 }
 
-const Loader = ({ status, focused }: { status: SearchStatus; focused: boolean }) => (
+const Loader = ({ status, isNavigating }: SearchButtonProps) => (
   <div
     className={clsx(styles.loader, {
-      [styles.loaderVisible]: [SearchStatus.Loading].includes(status),
-      [styles.loaderActive]: [SearchStatus.Loading].includes(status)
+      [styles.loaderActive]: isNavigating || status === SearchStatus.Loading
     })}
   >
     <LoaderHorseshoe />
   </div>
 )
 
-export const SearchButton = ({ status, focused, ...props }: SearchButtonProps) => {
-  return (
-    <div {...props} className={clsx(styles.container)}>
-      <Loader status={status} focused={focused} />
+export const SearchButton = (props: SearchButtonProps) => {
+  const { status, isNavigating, onClick, ...restProps } = props
 
-      <button className={clsx(styles.button)} disabled={status !== SearchStatus.Available}>
+  const isButtonDisabled = isNavigating || status !== SearchStatus.Available
+
+  return (
+    <div {...restProps} className={clsx(styles.container)}>
+      <Loader {...props} />
+
+      <button
+        onClick={onClick as MouseEventHandler<HTMLButtonElement> | undefined}
+        className={clsx(styles.button)}
+        disabled={isButtonDisabled}
+      >
         Register&nbsp;&nbsp;→
       </button>
     </div>
