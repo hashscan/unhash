771e64fb37ab3b125371846c1074221b67d36787
Alexey Taktarov <molefrog@gmail.com>
2023-02-17 12:42:36 +0100

    Favicon and basic meta tags.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import { createClient, WagmiConfig } from 'wagmi'
 import { Provider as WrapBalancerProvider } from 'react-wrap-balancer'
 import { NotifierProvider } from 'components/ui/Notifier/NotifierProvider'
 
-import '@rainbow-me/rainbowkit/styles.css'
-import 'styles/global.css'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import 'styles/global.css'
+import '@rainbow-me/rainbowkit/styles.css'
 import { chains, provider, connectors } from 'lib/connectors'
 import { wrapInLayout } from 'components/layouts'
 
@@ -25,6 +27,24 @@ const rainbowkitTheme: Theme = {
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <title>Get .ETH Domain For Your Ethereum Address</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+        <meta
+          name="description"
+          key="meta-description"
+          content={
+            'Simple tool for buying Etherium Name Service domains. ' +
+            'Set up your public .eth profile to stand out in Metaverse ' +
+            '(zero crypto knowledge required!)'
+          }
+        />
+      </Head>
+
       <style jsx global>
         {`
           :root {
