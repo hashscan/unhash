539750cbd0c6e5de10602ffec1a7c87bfbdeb4fa
Alex Tkachenko <skyalexx@gmail.com>
2023-02-17 01:36:37 +0000

    Send update records transaction

---
+++ lib/hooks/useSendUpdateRecords.ts
@@ -0,0 +1,63 @@
+import { BigNumber } from 'ethers'
+import { namehash } from 'ethers/lib/utils.js'
+import { ETH_RESOLVER_ABI, ETH_RESOLVER_ADDRESS } from 'lib/constants'
+import { Domain, DomainRecords, toNetwork } from 'lib/types'
+import {
+  useChainId,
+  useContract,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction
+} from 'wagmi'
+import { useNotifier } from './useNotifier'
+
+export const useSendUpdateRecords = ({
+  domain,
+  records
+}: {
+  domain: Domain
+  records: DomainRecords
+}) => {
+  const chainId = useChainId()
+  const notify = useNotifier() // TODO: handle by a hook user
+
+  const resolverAddress = ETH_RESOLVER_ADDRESS.get(toNetwork(chainId))
+  const contract = useContract({ abi: ETH_RESOLVER_ABI, address: resolverAddress })! // must always be defined
+
+  const node = domain ? namehash(domain) : undefined
+  const encoded = Object.entries(records).map(([key, record]) =>
+    contract?.interface.encodeFunctionData('setText', [node, key, record])
+  )
+
+  const { config } = usePrepareContractWrite({
+    address: resolverAddress,
+    abi: ETH_RESOLVER_ABI,
+    functionName: 'multicall',
+    enabled: encoded.length !== 0 && Boolean(domain),
+    args: [encoded],
+    overrides: {
+      gasLimit: BigNumber.from(250_000) // TODO: set proper gas limit
+    }
+  })
+
+  const {
+    write,
+    data,
+    error: sendError,
+    isLoading: isWriteLoading
+  } = useContractWrite({
+    ...config,
+    onError: (e) => notify(e.message, { status: 'error' })
+  })
+
+  const { isLoading: isWaitLoading, error: waitError } = useWaitForTransaction({
+    hash: data?.hash
+  })
+
+  return {
+    isLoading: isWriteLoading || isWaitLoading,
+    error: sendError || waitError,
+    sendUpdate: write,
+    gasLimit: config.request?.gasLimit
+  }
+}
