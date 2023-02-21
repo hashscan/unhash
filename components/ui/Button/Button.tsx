2bf65e3b899588dd87938742c5edbe68ea7d9630
Alexey Taktarov <molefrog@gmail.com>
2023-02-21 20:15:25 +0100

    Allow Button element customization via `as` prop.

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,31 +1,37 @@
 import clsx from 'clsx'
-import { ComponentProps } from 'react'
+import { ComponentProps, createElement, ForwardedRef } from 'react'
+import { forwardRef } from 'react'
 
 import { LoaderSpinner } from 'components/icons'
 
 import styles from './Button.module.css'
 
 export interface ButtonProps extends ComponentProps<'button'> {
+  as?: 'button' | 'a'
   size?: 'regular' | 'medium' | 'cta'
   isLoading?: boolean
 }
 
-export const Button = ({
-  size = 'regular',
-  isLoading = false,
-  children,
-  className,
-  ...rest
-}: ButtonProps) => {
-  return (
-    <button
-      {...rest}
-      className={clsx(className, styles.button, styles[`button_${size}`], {
-        [styles.button_loading]: isLoading
-      })}
-    >
+const ButtonWithRef = (
+  { as = 'button', size = 'regular', isLoading = false, children, className, ...rest }: ButtonProps,
+  ref: ForwardedRef<HTMLElement>
+) => {
+  const buttonProps = {
+    ref,
+    ...rest,
+    className: clsx(className, styles.button, styles[`button_${size}`], {
+      [styles.button_loading]: isLoading
+    })
+  }
+
+  return createElement(
+    as,
+    buttonProps,
+    <>
       <span className={styles.content}>{children}</span>
       {isLoading && <LoaderSpinner className={styles.loader} />}
-    </button>
+    </>
   )
 }
+
+export const Button = forwardRef(ButtonWithRef)
