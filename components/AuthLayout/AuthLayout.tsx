204d038eff668d44b4e90b462eec6cbd209b3d3c
Alexey Taktarov <molefrog@gmail.com>
2023-02-22 12:36:38 +0100

    Working Connect Wallet button in AuthLayout.

--- components/AuthLayout/AuthLayout.tsx
+++ components/AuthLayout/AuthLayout.tsx
@@ -2,11 +2,9 @@ import WrapBalancer from 'react-wrap-balancer'
 import styles from './AuthLayout.module.css'
 import { Button } from 'components/ui/Button/Button'
 
-export const AuthLayout = () => {
-  const onClick = () => {
-    // TODO: call wagmi connect
-  }
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
+export const AuthLayout = () => {
   return (
     <div className={styles.layout}>
       <div className={styles.header}>Connect your wallet</div>
@@ -15,9 +13,16 @@ export const AuthLayout = () => {
           Sign in with Metamask or Wallet&nbsp;Connect to view and edit your ENS profile
         </WrapBalancer>
       </div>
-      <Button className={styles.button} size={'medium'} onClick={onClick}>
-        Connect Wallet
-      </Button>
+
+      <ConnectButton.Custom>
+        {({ openConnectModal }) => {
+          return (
+            <Button className={styles.button} size={'medium'} onClick={openConnectModal}>
+              Connect Wallet
+            </Button>
+          )
+        }}
+      </ConnectButton.Custom>
     </div>
   )
 }
