26909a0c15b77f851361eaa5cc4b61fb4a5d41aa
Alex Tkachenko <skyalexx@gmail.com>
2023-02-24 15:41:05 +0000

    Feedback wrapper

--- components/Feedback/Feedback.tsx
+++ components/Feedback/Feedback.tsx
@@ -1,92 +1,11 @@
 import clsx from 'clsx'
-import { Button } from 'components/ui/Button/Button'
 import { ComponentProps, useState } from 'react'
 import styles from './Feedback.module.css'
-import { Input } from 'components/ui/Input/Input'
-import { FEEDBACK_TELEGRAM, FEEDBACK_TWITTER } from 'lib/constants'
-import api from 'lib/api'
-import { useNotifier } from 'lib/hooks/useNotifier'
+import { FeedbackForm } from './FeedbackForm'
 
-interface FeedbackProps extends ComponentProps<'div'> {
-  onCancel?: () => void
-  onSuccess?: () => void
-}
+export const Feedback = ({ className, ...rest }: ComponentProps<'div'>) => {
+  const [isOpen, setIsOpen] = useState<boolean>(true)
 
-export const Feedback = ({ className, ...rest }: FeedbackProps) => {
-  const notify = useNotifier()
-  const [author, setAuthor] = useState<string>('')
-  const [message, setMessage] = useState<string>('')
-
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [success, setSuccess] = useState<boolean>(false)
-
-  const onSend = async () => {
-    if (isLoading) return
-    try {
-      setIsLoading(true)
-      await api.saveFeedback(author, message)
-      setSuccess(true)
-    } catch (e) {
-      notify('Failed to send feedback', { status: 'error' })
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  // TODO: handle
-  if (success) return null
-
-  return (
-    <div className={clsx(className, styles.feedback)} {...rest}>
-      <div className={styles.content}>
-        <div className={styles.header}>{'📟 Your Feedback'}</div>
-        <div className={styles.subheader}>
-          Please share your thoughts on this beta app. Feel free to text us directly on{' '}
-          <a href={FEEDBACK_TWITTER} target="_blank" rel="noreferrer">
-            Twitter
-          </a>{' '}
-          or{' '}
-          <a href={FEEDBACK_TELEGRAM} target="_blank" rel="noreferrer">
-            Telegram
-          </a>
-          . Thank you!
-        </div>
-        <Input
-          className={styles.twitter}
-          label="Your Twitter / Telegram"
-          placeholder="@mastodon (Optional)"
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <div className={styles.label}>Message</div>
-        <textarea
-          className={styles.form}
-          placeholder={
-            'Did you like the app? Share your ideas on what we should support and improve.'
-          }
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <div className={styles.buttons}>
-        {/* <Button
-          className={styles.buttonCancel}
-          disabled={false}
-          isLoading={false}
-          as={'button'}
-          size={'regular'}
-          onClick={onCancel}
-        >
-          Cancel
-        </Button> */}
-        <Button
-          className={styles.buttonSend}
-          disabled={message.length === 0}
-          isLoading={isLoading}
-          size={'regular'}
-          onClick={onSend}
-        >
-          Send Feedback
-        </Button>
-      </div>
-    </div>
-  )
+  if (!isOpen) return null
+  return <FeedbackForm className={clsx(className, styles.feedback)} />
 }
