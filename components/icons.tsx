f0a146f192223ca00a7ee126ffd6e8264a823aa2
Alex Tkachenko <skyalexx@gmail.com>
2023-02-24 15:54:27 +0000

    Feedback menu

--- components/icons.tsx
+++ components/icons.tsx
@@ -273,6 +273,28 @@ export const FaceWink = (props: BaseIconProps) => (
   </BaseIcon>
 )
 
+export const Chat = (props: BaseIconProps) => (
+  <BaseIcon {...props} baseSize={24}>
+    <svg version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <g
+        stroke-linecap="round"
+        stroke-width="1.5"
+        stroke="currentColor"
+        fill="none"
+        stroke-linejoin="round"
+      >
+        <path d="M15.5,14.5h-7"></path>
+        <path d="M8.5,10.5h7"></path>
+        <path
+          stroke-width="1.5882"
+          d="M4.151,16.396c-0.73,-1.3 -1.151,-2.798 -1.151,-4.396c0,-4.971 4.029,-9 9,-9c4.971,0 9,4.029 9,9c0,4.971 -4.029,9 -9,9c-1.598,0 -3.096,-0.421 -4.396,-1.151l-4.604,1.151l1.151,-4.604Z"
+        ></path>
+      </g>
+      <path fill="none" d="M0,0h24v24h-24v-24Z"></path>
+    </svg>
+  </BaseIcon>
+)
+
 /*
  * <BaseIcon />
  */
