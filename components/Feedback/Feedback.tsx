a4d684563f8fe7bb1e544d32ea55c10d9d832f28
Alex Tkachenko <skyalexx@gmail.com>
2023-02-24 15:54:27 +0000

    Feedback menu

--- components/Feedback/Feedback.tsx
+++ components/Feedback/Feedback.tsx
@@ -1,11 +1,20 @@
 import clsx from 'clsx'
+import { Chat } from 'components/icons'
 import { ComponentProps, useState } from 'react'
 import styles from './Feedback.module.css'
 import { FeedbackForm } from './FeedbackForm'
 
 export const Feedback = ({ className, ...rest }: ComponentProps<'div'>) => {
-  const [isOpen, setIsOpen] = useState<boolean>(true)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
-  if (!isOpen) return null
-  return <FeedbackForm className={clsx(className, styles.feedback)} />
+  if (!isOpen)
+    return (
+      <div
+        className={clsx(className, styles.feedback, styles.menu)}
+        onClick={() => setIsOpen(true)}
+      >
+        <Chat />
+      </div>
+    )
+  return <FeedbackForm className={clsx(className, styles.feedback)} {...rest} />
 }
