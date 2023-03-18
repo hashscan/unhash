fddd32c5a53f44d6631b6aec30c6a9c02e455977
Alex Tkachenko <skyalexx@gmail.com>
2023-03-18 18:31:21 +0000

    Empty /names page

---
+++ pages/names.tsx
@@ -0,0 +1,39 @@
+import { useAccount } from 'wagmi'
+import styles from './profile.module.css'
+import { ContainerLayout, PageWithLayout } from 'components/layouts'
+import { AuthLayout } from 'components/AuthLayout/AuthLayout'
+import { useCurrentUserInfo } from 'lib/hooks/useUserInfo'
+import { useMemo, useState } from 'react'
+import { LoaderSpinner } from 'components/icons'
+
+const Names: PageWithLayout = () => {
+  const { address, isDisconnected } = useAccount()
+
+  const userInfo = useCurrentUserInfo()
+  const userDomains = useMemo(() => userInfo?.domains.filter((d) => d.isValid) || [], [userInfo])
+
+  // TODO: handle isConnecting state when metamask asked to log in
+  if (isDisconnected) return <AuthLayout />
+
+  // loader
+  if (!userInfo) {
+    return (
+      <div className={styles.loading}>
+        <LoaderSpinner className={styles.loader} />
+      </div>
+    )
+  }
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.title}>My names</div>
+
+      <div className={styles.header}>All names</div>
+      <div className={styles.subheader}>This is all names you own or control.</div>
+    </main>
+  )
+}
+
+Names.layout = <ContainerLayout verticalPadding={false} />
+
+export default Names
