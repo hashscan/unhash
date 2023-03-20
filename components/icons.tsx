ce0d5e5577b446c63c0af093929784349f6ec679
Alex Tkachenko <skyalexx@gmail.com>
2023-03-20 23:43:53 +0000

    Add menu placeholder

--- components/icons.tsx
+++ components/icons.tsx
@@ -295,6 +295,26 @@ export const Chat = (props: BaseIconProps) => (
   </BaseIcon>
 )
 
+export const Menu = (props: BaseIconProps) => (
+  <BaseIcon {...props} baseSize={24}>
+    <svg
+      fill="none"
+      height="18"
+      shape-rendering="geometricPrecision"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+      viewBox="0 0 24 24"
+      width="18"
+    >
+      <circle cx="12" cy="12" r="1" fill="currentColor"></circle>
+      <circle cx="12" cy="5" r="1" fill="currentColor"></circle>
+      <circle cx="12" cy="19" r="1" fill="currentColor"></circle>
+    </svg>
+  </BaseIcon>
+)
+
 /*
  * <BaseIcon />
  */
