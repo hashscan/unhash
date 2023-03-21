b19dc88b5b1d52b3afcc90d6f2a815f25e6e123f
Alex Tkachenko <skyalexx@gmail.com>
2023-03-21 15:26:09 +0000

    Select all

--- pages/names.tsx
+++ pages/names.tsx
@@ -3,7 +3,7 @@ import styles from './names.module.css'
 import { ContainerLayout, PageWithLayout } from 'components/layouts'
 import { AuthLayout } from 'components/AuthLayout/AuthLayout'
 import { useCurrentUserInfo } from 'lib/hooks/useUserInfo'
-import { useMemo, useState } from 'react'
+import { useCallback, useContext, useMemo, useState } from 'react'
 import { LoaderSpinner, Menu, Search } from 'components/icons'
 import clsx from 'clsx'
 import Link from 'next/link'
@@ -13,31 +13,51 @@ import Checkbox from 'components/ui/Checkbox/Checkbox'
 const Names: PageWithLayout = () => {
   const { isDisconnected } = useAccount()
 
-  const userInfo = useCurrentUserInfo()
-
+  // search input filter
   const [filter, setFilter] = useState('')
-  const [selectedNames, setSelectedNames] = useState<string[]>([])
-
-  const onCheckChange = (name: string, checked: boolean) => {
-    if (checked) {
-      setSelectedNames([...selectedNames, name])
-    } else {
-      setSelectedNames(selectedNames.filter((n) => n !== name))
-    }
+  const onFilterChange = (input: string) => {
+    setFilter(input)
+    setSelectedNames([]) // clear selection for simplicity
   }
 
-  // get owned and controlled domains
-  const domains = useMemo(
+  // domain list
+  const userInfo = useCurrentUserInfo()
+  const allDomains = useMemo(
     () =>
       userInfo?.domains
-        .filter(
-          (d) =>
-            d.isValid &&
-            (d.controlled || d.owned) &&
-            d.name.toLowerCase().includes(filter.toLowerCase())
-        )
+        .filter((d) => d.isValid && (d.controlled || d.owned))
         .sort((a, b) => a.name.localeCompare(b.name)) || [],
-    [userInfo, filter]
+    [userInfo]
+  )
+
+  const filteredDomains = useMemo(
+    () => allDomains.filter((d) => d.name.toLowerCase().includes(filter.toLowerCase())) || [],
+    [allDomains, filter]
+  )
+
+  // checkbox selection
+  const [selectedNames, setSelectedNames] = useState<string[]>([])
+  const onCheckChange = useCallback(
+    (name: string, checked: boolean) => {
+      if (checked) {
+        setSelectedNames([...selectedNames, name])
+      } else {
+        setSelectedNames(selectedNames.filter((n) => n !== name))
+      }
+    },
+    [selectedNames, setSelectedNames]
+  )
+
+  const allSelected = selectedNames.length === filteredDomains.length
+  const onCheckAllChange = useCallback(
+    (checked: boolean) => {
+      if (checked) {
+        setSelectedNames(filteredDomains.map((d) => d.name))
+      } else {
+        setSelectedNames([])
+      }
+    },
+    [filteredDomains, setSelectedNames]
   )
 
   // TODO: handle isConnecting state when metamask asked to log in
@@ -70,60 +90,65 @@ const Names: PageWithLayout = () => {
           autoComplete="false"
           placeholder="Search..."
           value={filter}
-          onChange={(e) => setFilter(e.target.value)}
+          onChange={(e) => onFilterChange(e.target.value)}
         />
       </div>
 
-      <table className={styles.table}>
-        <thead>
-          <tr className={clsx(styles.row)}>
-            <th className={clsx(styles.cell, styles.headCell, styles.selectCell)}>
-              <div className={styles.checkboxContainer}>
-                <div className={styles.checkbox} />
-              </div>
-            </th>
-            <th className={clsx(styles.cell, styles.headCell, styles.nameCell)}>Domain</th>
-            <th className={clsx(styles.cell, styles.headCell, styles.rightsCell)}>Rights</th>
-            <th className={clsx(styles.cell, styles.headCell, styles.expirationCell)}>
-              Expiration
-            </th>
-            <th className={clsx(styles.cell, styles.headCell, styles.menuCell)} />
-          </tr>
-        </thead>
-        <tbody>
-          {domains.map((domain) => (
-            <tr key={domain.name} className={styles.row}>
-              <td className={clsx(styles.cell, styles.selectCell)}>
+      {filteredDomains.length > 0 && (
+        <table className={styles.table}>
+          <thead>
+            <tr className={clsx(styles.row)}>
+              <th className={clsx(styles.cell, styles.headCell, styles.selectCell)}>
                 <div className={styles.checkboxContainer}>
                   <Checkbox
-                    checked={selectedNames.includes(domain.name)}
-                    onChange={(e) => onCheckChange(domain.name, e.target.checked)}
+                    checked={allSelected}
+                    onChange={(e) => onCheckAllChange(e.target.checked)}
                   />
                 </div>
-              </td>
-              <td className={clsx(styles.cell, styles.nameCell)}>
-                <Link className={styles.domain} href={`/${domain.name}/`} target="_blank">
-                  {domain.name}
-                </Link>
-              </td>
-              <td className={clsx(styles.cell, styles.rightsCell)}>
-                <div className={styles.rights}>
-                  {domain.owned && <div className={styles.right}>Owner</div>}
-                  {domain.controlled && <div className={styles.right}>Controller</div>}
-                </div>
-              </td>
-              <td className={clsx(styles.cell, styles.expirationCell)}>
-                {domain.expiresAt ? formatExpiresIn(domain.expiresAt) : ''}
-              </td>
-              <td className={clsx(styles.cell, styles.menuCell)}>
-                <div className={styles.menuContainer}>
-                  <Menu />
-                </div>
-              </td>
+              </th>
+              <th className={clsx(styles.cell, styles.headCell, styles.nameCell)}>Domain</th>
+              <th className={clsx(styles.cell, styles.headCell, styles.rightsCell)}>Rights</th>
+              <th className={clsx(styles.cell, styles.headCell, styles.expirationCell)}>
+                Expiration
+              </th>
+              <th className={clsx(styles.cell, styles.headCell, styles.menuCell)} />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredDomains.map((domain) => (
+              <tr key={domain.name} className={styles.row}>
+                <td className={clsx(styles.cell, styles.selectCell)}>
+                  <div className={styles.checkboxContainer}>
+                    <Checkbox
+                      checked={selectedNames.includes(domain.name)}
+                      onChange={(e) => onCheckChange(domain.name, e.target.checked)}
+                    />
+                  </div>
+                </td>
+                <td className={clsx(styles.cell, styles.nameCell)}>
+                  <Link className={styles.domain} href={`/${domain.name}/`} target="_blank">
+                    {domain.name}
+                  </Link>
+                </td>
+                <td className={clsx(styles.cell, styles.rightsCell)}>
+                  <div className={styles.rights}>
+                    {domain.owned && <div className={styles.right}>Owner</div>}
+                    {domain.controlled && <div className={styles.right}>Controller</div>}
+                  </div>
+                </td>
+                <td className={clsx(styles.cell, styles.expirationCell)}>
+                  {domain.expiresAt ? formatExpiresIn(domain.expiresAt) : ''}
+                </td>
+                <td className={clsx(styles.cell, styles.menuCell)}>
+                  <div className={styles.menuContainer}>
+                    <Menu />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </main>
   )
 }
