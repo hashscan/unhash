ac2b9be14c72637390f58a7030271fe9801fb5ee
Alex Tkachenko <skyalexx@gmail.com>
2023-03-23 18:16:09 +0000

    Fix address input on unfocus

--- components/ui/AddressInput/AddressInput.tsx
+++ components/ui/AddressInput/AddressInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Input, InputProps } from '../Input/Input'
 import { isValidAddress } from 'lib/utils'
 import useEvent from 'react-use-event-hook'
@@ -7,36 +7,27 @@ export interface AddressInputProps extends InputProps {
   onAddressChange?: (address: string | null) => void // empty string - not set, null - invalid input
 }
 
-// TODO: support a way to trigger validation from parent
-// TODO: support error message
 // Input with built-in address validation
 export const AddressInput = ({ onAddressChange, onChange, onBlur, ...rest }: AddressInputProps) => {
-  const [address, setAddress] = useState<string>('')
+  const [address, setAddress] = useState<string | null>('') // empty string - not set, null - invalid input
   const [showError, setShowError] = useState<boolean>(false)
 
   // just to ensure that we don't do extra re-renders
   const onChangeStable = useEvent((value) => onAddressChange?.(value))
-  const isValid = isValidAddress(address)
-
-  useEffect(() => {
-    if (isValid) {
-      onChangeStable(address)
-    } else {
-      onChangeStable(null)
-    }
-  }, [isValid, address, onChangeStable])
 
   return (
     <Input
-      error={showError && !isValid ? 'Invalid address' : undefined}
+      error={showError ? 'Invalid address' : undefined}
       onBlur={(e) => {
-        setShowError(true)
+        setShowError(address === null)
         onBlur?.(e)
       }}
-      value={address}
+      value={address ?? ''}
       onChange={(e) => {
-        setAddress(e.target.value)
         setShowError(false)
+        const value = e.target.value
+        setAddress(value === '' || isValidAddress(value) ? value : null)
+        onChangeStable(address)
         onChange?.(e)
       }}
       {...rest}
