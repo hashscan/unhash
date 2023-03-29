81d49da51bae7dcb45bb08244043150c88f4f8a0
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-03-29 18:45:43 +0300

    make draft modal

---
+++ components/UnfinishedRegistrationWarning/UnfinishedRegistrationWarning.tsx
@@ -0,0 +1,45 @@
+import { Button } from 'components/ui/Button/Button'
+import { Registration } from 'lib/types'
+
+import styles from './UnfinishedRegistrationWarning.module.css'
+
+type UnfinishedRegistrationWarningProps = {
+  registration: Registration
+  onCancel: () => void
+  onContinue: () => void
+}
+
+export const UnfinishedRegistrationWarning = ({
+  registration,
+  onCancel,
+  onContinue
+}: UnfinishedRegistrationWarningProps) => {
+  return (
+    <>
+      <div className={styles.backdrop} />
+      <div className={styles.overlay}>
+        <div className={styles.modal}>
+          <div className={styles.body}>
+            <div className={styles.title}>Unfinished registration</div>
+            <div className={styles.text}>
+              You have not finished registering the <strong>{registration.names.join(', ')}</strong>{' '}
+              {registration.names.length === 1 ? 'name' : 'names'}. Would you like to continue the
+              registration process? If you skip this step, you might not be able to finalize it
+              later on.
+            </div>
+          </div>
+
+          <div className={styles.footer}>
+            <Button onClick={onCancel} size={'regular'} variant={'ghost'}>
+              Cancel
+            </Button>
+
+            <Button size={'regular'} disabled={false} onClick={onContinue}>
+              Continue
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
