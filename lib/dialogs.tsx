4875ba5816acb7904d7725330333b2dd0d1a8a5d
Alexey Taktarov <molefrog@gmail.com>
2023-03-30 10:04:48 +0200

    Imperative API for controlling dialogs.

--- lib/dialogs.tsx
+++ lib/dialogs.tsx
@@ -1,17 +1,45 @@
 import { SetAvatarDialog } from 'components/SetAvatarDialog/SetAvatarDialog'
-import { useState } from 'react'
+import { createNanoEvents } from 'nanoevents'
+import { useEffect, useState } from 'react'
 
-export type DialogName = 'setAvatar' | 'sendDomain' | 'renewDomain'
+/**
+ * When adding new dialogs to the app, register their components here!
+ */
+const DialogComponents = {
+  setAvatar: SetAvatarDialog,
+  sendDomain: () => <>todo!</>
+} as const
+
+export type DialogName = keyof typeof DialogComponents
+
+interface Events {
+  openDialog: (type: DialogName) => void
+  closeDialog: () => void
+}
+
+const mediator = createNanoEvents<Events>()
+
+// imperative API for controlling dialogs
+export const openDialog = (type: DialogName) => mediator.emit('openDialog', type)
+export const closeDialog = () => mediator.emit('closeDialog')
 
 export const Dialogs = () => {
   const [currentDialog, setCurrentDialog] = useState<DialogName | null>(null)
 
+  useEffect(() => {
+    mediator.on('openDialog', (type) => setCurrentDialog(type))
+    mediator.on('closeDialog', () => setCurrentDialog(null))
+
+    return () => {
+      mediator.events = {}
+    } // clear all subscribers
+  }, [])
+
   return (
     <>
-      <SetAvatarDialog
-        open={currentDialog === 'setAvatar'}
-        onClose={() => setCurrentDialog(null)}
-      />
+      {Object.entries(DialogComponents).map(([name, Dialog]) => (
+        <Dialog key={name} open={currentDialog === name} onClose={() => setCurrentDialog(null)} />
+      ))}
     </>
   )
 }
