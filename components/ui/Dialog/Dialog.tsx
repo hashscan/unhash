11c196a1a609330f807ce6e5ae8a8e732a42c03b
Alexey Taktarov <molefrog@gmail.com>
2023-03-30 10:30:04 +0200

    Animate dialog transitions.

--- components/ui/Dialog/Dialog.tsx
+++ components/ui/Dialog/Dialog.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import { Dialog as HeadlessDialog } from '@headlessui/react'
 import { type DialogProps as HeadlessDialogProps } from '@headlessui/react'
+import { AnimatePresence, motion } from 'framer-motion'
 import clsx from 'clsx'
 
 import styles from './Dialog.module.css'
@@ -34,14 +35,92 @@ export const Dialog = ({
   }
 
   return (
-    <HeadlessDialog className={styles.container} open={open} onClose={handleClose}>
-      {/* Full-screen container to center the panel */}
-      <div className={styles.backdrop}>
-        <HeadlessDialog.Panel className={clsx(styles.panel, styles[`panel_${size}`], className)}>
-          {children}
-          {footer && <div className={styles.footer}>{footer}</div>}
-        </HeadlessDialog.Panel>
-      </div>
-    </HeadlessDialog>
+    <AnimatePresence>
+      {open && (
+        <HeadlessDialog
+          as={motion.div}
+          static
+          className={styles.container}
+          open={open}
+          onClose={handleClose}
+          variants={containerAnimationVariants}
+        >
+          {/* Full-screen container to center the panel */}
+          <motion.div
+            variants={backdropAnimationVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            className={styles.backdrop}
+          >
+            <HeadlessDialog.Panel
+              className={clsx(styles.panel, styles[`panel_${size}`], className)}
+              as={motion.div}
+              variants={panelAnimationVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              {children}
+              {footer && <div className={styles.footer}>{footer}</div>}
+            </HeadlessDialog.Panel>
+          </motion.div>
+        </HeadlessDialog>
+      )}
+    </AnimatePresence>
   )
+}
+
+/**
+ * Animation states for backdrop and the dialog
+ */
+const containerAnimationVariants = {
+  visible: {},
+  hidden: {},
+  exit: {}
+}
+
+const backdropAnimationVariants = {
+  visible: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backdropFilter: 'blur(2px)',
+    transition: {
+      duration: 0.2
+    }
+  },
+
+  hidden: {
+    backgroundColor: 'rgba(0, 0, 0, 0.0)',
+    backdropFilter: 'blur(0px)',
+    transition: {
+      duration: 0.2,
+      delay: 0.25
+    }
+  }
+}
+
+const panelAnimationVariants = {
+  visible: {
+    scale: 1.0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5
+    }
+  },
+
+  hidden: {
+    scale: 0.9
+  },
+
+  exit: {
+    opacity: 0,
+    scale: 1.05,
+    translateY: 20,
+    filter: 'blur(8px)',
+    transition: {
+      duration: 0.2
+    }
+  }
 }
