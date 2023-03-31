40d0b85d2543527fd4934279d87a89ecf6b1c5bc
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-03-31 11:52:01 +0300

    fix typescript

--- pages/register.tsx
+++ pages/register.tsx
@@ -77,7 +77,7 @@ const Register: PageWithLayout<RegisterProps> = ({ names }: RegisterProps) => {
           {step === 'initializing' && <div></div>}
           {step === 'commit' && <CheckoutCommitStep order={order} updateOrder={updateOrder} />}
           {step === 'wait' && reg && <CheckoutWaitStep registration={reg} />}
-          {step === 'register' && reg && <CheckoutRegisterStep />}
+          {step === 'register' && reg && <CheckoutRegisterStep registration={reg} />}
           {step === 'success' && reg && <CheckoutSuccessStep domain={domain} registration={reg} />}
         </main>
 
