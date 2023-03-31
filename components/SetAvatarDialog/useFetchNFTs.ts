2a2de15c7e9a9fa7f252a8ba6d3579a48c0a8104
Alexey Taktarov <molefrog@gmail.com>
2023-03-31 10:26:08 +0200

    Remove goerli workaround.

--- components/SetAvatarDialog/useFetchNFTs.ts
+++ components/SetAvatarDialog/useFetchNFTs.ts
@@ -10,13 +10,6 @@ export const useFetchNFTs = (limit: number, continuation?: string) => {
   const { address } = useAccount()
   const network = toNetwork(useChainId())
 
-  // the API currently does not support fetching NFTs from Goerli, so we just use a
-  // placeholder address for now
-  if (network === 'goerli') {
-    return () =>
-      api.userNFTs('mainnet', '0xB4b18818E9262584921b371c891b62219DaefeA3', limit, continuation)
-  }
-
   return async () => {
     if (!address) throw new Error('No address provided')
     return await api.userNFTs(network, address, limit, continuation)
