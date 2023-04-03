3501b9c67f1eceeaf41a509b1a8fb89d4d4a7e1f
Alexey Taktarov <molefrog@gmail.com>
2023-04-03 15:18:38 +0200

    Display avatar preview when transaction is in progress.

---
+++ components/SetAvatarDialog/Preview.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+
+import styles from './Preview.module.css'
+import clsx from 'clsx'
+
+import { formatAddress } from 'lib/utils'
+
+type PreviewProps = {
+  visible: boolean
+  avatarImg?: string
+  name?: string
+  address?: string
+}
+
+export const Preview = ({ visible, avatarImg, name, address }: PreviewProps) => {
+  return (
+    <div className={clsx(styles.preview, { [styles.preview_visible]: visible })}>
+      <div className={styles.account}>
+        <div className={styles.avatar}>
+          {/* we don't want to use image optimization for external images here */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          {avatarImg && <img src={avatarImg} alt={`ENS Avatar for ${address}`} />}
+        </div>
+
+        <div className={styles.info}>
+          {name && <div className={styles.ensName}>{name}</div>}
+          {address && <div className={styles.address}>{formatAddress(address, 6)}</div>}
+        </div>
+      </div>
+    </div>
+  )
+}
