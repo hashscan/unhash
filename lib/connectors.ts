cab42e74c6884d1df56b227be36080981c99979d
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-04-11 12:06:05 +0300

    use infura as a backup provider

--- lib/connectors.ts
+++ lib/connectors.ts
@@ -6,14 +6,15 @@ import {
   walletConnectWallet
 } from '@rainbow-me/rainbowkit/wallets'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { infuraProvider } from 'wagmi/providers/infura'
 import { configureChains } from 'wagmi'
 import { currentNetwork, toChain } from './network'
 
 const { chains, provider } = configureChains(
   [toChain(currentNetwork())],
   [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!, priority: 0 })
-    // no backup provider on purpose
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!, priority: 0 }),
+    infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_API_KEY! })
   ]
 )
 
