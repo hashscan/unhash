daba807ad93af71ea2400d71dae31a6ee503f9b7
Alex Tkachenko <skyalexx@gmail.com>
2023-04-18 19:11:15 +0100

    Update local storage keys

--- lib/hooks/useCart.tsx
+++ lib/hooks/useCart.tsx
@@ -2,5 +2,5 @@ import { useLocalStorage } from 'usehooks-ts'
 import { Cart } from 'lib/types'
 
 export const useCart = () => {
-  return useLocalStorage<Cart>('unhash_names', { names: [] })
+  return useLocalStorage<Cart>('unhash.cart', { names: [] })
 }
