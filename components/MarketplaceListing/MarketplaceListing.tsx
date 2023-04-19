1c9937ce626ea28a74e72df191a9f09ea4c2a353
Alexey Taktarov <molefrog@gmail.com>
2023-04-19 19:55:14 +0200

    Temporarily fix MarketplaceListing.

--- components/MarketplaceListing/MarketplaceListing.tsx
+++ components/MarketplaceListing/MarketplaceListing.tsx
@@ -1,18 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { ComponentProps, useCallback, useEffect } from 'react'
-import styles from './BuyCard.module.css'
+import styles from './MarketplaceListing.module.css'
 import clsx from 'clsx'
 import { DomainListing } from 'lib/types'
 import { formatUSDPrice } from 'lib/format'
 import { trackGoal } from 'lib/analytics'
-import { Button } from '../Button/Button'
+import { Button } from 'components/ui/Button/Button'
 
-export interface BuyCardProps extends ComponentProps<'div'> {
+export interface MarketplaceListingProps extends ComponentProps<'div'> {
   name: string
   listing: DomainListing
 }
 
-export const BuyCard = ({ name, listing, className, ...rest }: BuyCardProps) => {
+export const MarketplaceListing = ({
+  name,
+  listing,
+  className,
+  ...rest
+}: MarketplaceListingProps) => {
   useEffect(() => {
     trackGoal('BuyCardView', { props: { name: name, source: listing.source.name } })
   }, [name, listing])
