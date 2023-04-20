d2f0d3d55ee4e73667b12015fe4b0f143ad60d6d
Alex Tkachenko <skyalexx@gmail.com>
2023-04-20 22:38:14 +0100

    Move icon

--- components/LandingBulk/LandingBulk.tsx
+++ components/LandingBulk/LandingBulk.tsx
@@ -4,14 +4,16 @@ import styles from './LandingBulk.module.css'
 export const LandingBulk = () => {
   return (
     <div className={styles.bulk}>
-      <div className={styles.logo}>
-        <Basket width={56} height={56} />
-      </div>
       <div className={styles.content}>
-        <div className={styles.header}>Bulk register and renew</div>
-        <div className={styles.text}>
-          Seamlessly register and renew multiple names at once in just 2 transactions. Save on gas
-          fees and time.
+        <div className={styles.logo}>
+          <Basket width={56} height={56} />
+        </div>
+        <div>
+          <div className={styles.header}>Bulk register and renew</div>
+          <div className={styles.text}>
+            Seamlessly register and renew multiple names at once in just 2 transactions. Save on gas
+            fees and time.
+          </div>
         </div>
       </div>
 
