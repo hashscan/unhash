43d8ecc8fe44dc7cafb6aa328240139f5c547512
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-09 20:43:10 +0300

    posts

---
+++ mdx.d.ts
@@ -0,0 +1,14 @@
+let MDXComponent: (props) => JSX.Element
+
+declare module './content/*.mdx' {
+  export const title: string
+  export const description: string
+  export const slug: string
+  export const preview: string
+
+  export default MDXComponent
+}
+
+declare module '*.mdx' {
+  export default MDXComponent
+}
