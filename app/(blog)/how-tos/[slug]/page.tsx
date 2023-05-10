6c543254f1893ab38644136214f69011da24cc11
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-10 13:15:14 +0300

    try canary release

--- app/(blog)/how-tos/[slug]/page.tsx
+++ app/(blog)/how-tos/[slug]/page.tsx
@@ -1,20 +1,12 @@
 import { notFound } from 'next/navigation'
-
-import post$1 from 'content/first-post.mdx'
-import post$2 from 'content/second-post.mdx'
-
-const getPosts = async () =>
-  ({
-    'first-post': post$1,
-    'second-post': post$2
-  } as Record<string, typeof post$1>)
+import posts from 'content'
 
 export async function generateStaticParams() {
-  return Object.keys(await getPosts())
+  return Object.keys(posts)
 }
 
 export default async function Post({ params }: { params: { slug: string } }) {
-  const Content = (await getPosts())[params.slug]
+  const { default: Content } = posts[params.slug]
 
   if (!Content) {
     notFound()
