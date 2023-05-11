667ba4ada68384be55dc3935b0657fe364ac41f2
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-11 16:01:29 +0300

    disallow all pages when it is not prod

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,17 @@
 export default function robots() {
+  const isProduction = !!process.env.NEXT_PUBLIC_PRODUCTION_URL
   const hostname = process.env.NEXT_PUBLIC_PRODUCTION_URL ?? 'https://unhash.com'
+
+  if (!isProduction) {
+    return {
+      rules: [
+        {
+          userAgent: '*',
+          disallow: '/'
+        }
+      ]
+    }
+  }
 
   return {
     rules: [
@@ -7,7 +19,7 @@ export default function robots() {
         userAgent: '*'
       }
     ],
-    sitemap: '${hostname}/sitemap.xml',
+    sitemap: `${hostname}/sitemap.xml`,
     host: hostname
   }
 }
