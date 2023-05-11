0519584191aed5c1f1417601d3b4a5a51b5faf8d
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-11 15:22:29 +0300

    return active links

--- components/Nav/Links.tsx
+++ components/Nav/Links.tsx
@@ -3,19 +3,20 @@ import clsx from 'clsx'
 import Link from 'next/link'
 
 import styles from './Links.module.css'
-// import { useCurrentRoute } from 'lib/hooks/useCurrentRoute'
 
-export const Links = (props: ComponentPropsWithoutRef<'div'> & { mobile?: boolean }) => {
-  const { mobile, ...rest } = props
-  // const route = useCurrentRoute()
+export const Links = (
+  props: ComponentPropsWithoutRef<'div'> & { mobile?: boolean; usePathname: () => string }
+) => {
+  const { mobile, usePathname, ...rest } = props
+  const route = usePathname()
 
   return (
     <div {...rest} className={clsx(props.className, styles.links)}>
       <Link
         href="/"
         className={clsx(styles.navLink, {
-          [styles['navLink_mobile']]: mobile
-          // [styles.navLinkActive]: route === '/' || route.match(/\/register$/) !== null
+          [styles['navLink_mobile']]: mobile,
+          [styles.navLinkActive]: route === '/' || route.match(/\/register$/) !== null
         })}
       >
         Search
@@ -24,8 +25,8 @@ export const Links = (props: ComponentPropsWithoutRef<'div'> & { mobile?: boolea
       <Link
         href="/names"
         className={clsx(styles.navLink, {
-          [styles['navLink_mobile']]: mobile
-          // [styles.navLinkActive]: route === '/names'
+          [styles['navLink_mobile']]: mobile,
+          [styles.navLinkActive]: route === '/names'
         })}
       >
         My names
@@ -34,8 +35,8 @@ export const Links = (props: ComponentPropsWithoutRef<'div'> & { mobile?: boolea
       <Link
         href="/profile"
         className={clsx(styles.navLink, {
-          [styles['navLink_mobile']]: mobile
-          // [styles.navLinkActive]: route === '/profile'
+          [styles['navLink_mobile']]: mobile,
+          [styles.navLinkActive]: route === '/profile'
         })}
       >
         Profile
