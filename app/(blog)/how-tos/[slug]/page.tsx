e0c82ad5c670614b62c701f24d978a51ff1f1ed6
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-11 15:51:01 +0300

    add more metadata for blog post

--- app/(blog)/how-tos/[slug]/page.tsx
+++ app/(blog)/how-tos/[slug]/page.tsx
@@ -10,13 +10,38 @@ export function generateStaticParams() {
 type PostParams = { params: { slug: string } }
 
 export function generateMetadata({ params }: PostParams) {
+  const isProduction = !!process.env.NEXT_PUBLIC_PRODUCTION_URL
+  const hostname = process.env.NEXT_PUBLIC_PRODUCTION_URL ?? 'https://unhash.com'
   const post = posts[params.slug]
 
   const metadata: Metadata = {
     title: post.title,
     description: post.description,
+    keywords: [],
 
-    robots: 'index, follow'
+    robots: isProduction ? 'index, follow' : 'noindex, nofollow',
+    
+    referrer: 'origin',
+    icons: '/favicon.svg',
+
+    openGraph: {
+      type: 'website',
+      url: `${hostname}/how-tos/${post.slug}`,
+      title: post.title,
+      description: post.description,
+      images: [
+        {
+          url: 'og-preview.jpg'
+        }
+      ]
+    },
+
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.description,
+      images: 'og-preview.jpg'
+    }
   }
 
   return metadata
