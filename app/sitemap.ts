ffdad61112b12e4d03563955ee1d79954e1dceca
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-11 15:40:47 +0300

    add sitemap.xml

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import posts from 'content'
+
+export default async function sitemap() {
+  const hostname = process.env.NEXT_PUBLIC_PRODUCTION_URL ?? 'https://unhash.com/'
+
+  const postUrls = Object.values(posts).map((post) => `how-tos/${post.slug}`)
+
+  return ['', 'names', 'profile', ...postUrls].map((route) => ({
+    url: `${hostname}/${route}`,
+    lastModified: new Date().toISOString().split('T')[0]
+  }))
+}
